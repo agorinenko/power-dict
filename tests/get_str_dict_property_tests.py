e90b162ae2de33463af9e5f9cd0a85b7d36271c8
anton <anton.gorinenko@gmail.com>
2020-03-27 13:27:27 +0300

    0.0.11

--- tests/get_str_dict_property_tests.py
+++ tests/get_str_dict_property_tests.py
@@ -7,7 +7,8 @@ from power_dict.utils import DictUtils
 class GetStrDictPropertyTests(unittest.TestCase):
     properties = {
         "property_1": "Hello!",
-        "property_1_none": None
+        "property_1_none": None,
+        "property_1_empty": ''
     }
 
     def test_get_property(self):
@@ -16,6 +17,10 @@ class GetStrDictPropertyTests(unittest.TestCase):
         self.assertEqual(target, "Hello!")
 
         target = DictUtils.get_str_dict_property(self.properties, 'property_1_none', default_value="Default string")
+        self.assertIsInstance(target, str)
+        self.assertEqual(target, "Default string")
+
+        target = DictUtils.get_str_dict_property(self.properties, 'property_1_empty', default_value="Default string")
         self.assertIsInstance(target, str)
         self.assertEqual(target, "Default string")
 
@@ -31,6 +36,10 @@ class GetStrDictPropertyTests(unittest.TestCase):
         with self.assertRaises(NoneParameterError):
             DictUtils.get_required_str_dict_property(self.properties, 'property_1_none',
                                                      required_error="Key property_1_none is None")
+
+            with self.assertRaises(NoneParameterError):
+                DictUtils.get_required_str_dict_property(self.properties, 'property_1_empty',
+                                                         required_error="Key property_1_none is None")
 
         with self.assertRaises(NoneParameterError):
             DictUtils.get_required_str_dict_property(self.properties, 'key_not_found')
